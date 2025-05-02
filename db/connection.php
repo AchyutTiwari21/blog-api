c449b0e99608b5cae68429b91ca518f5bb6aaad8
AchyutTiwari21 <achyut.s.tiwari@gmail.com>
2025-05-02 16:38:28 +0530

    Update file according to production.

--- db/connection.php
+++ db/connection.php
@@ -1,9 +1,14 @@
 <?php
+$host = "sql101.infinityfree.com";    
+$dbname = "if0_38872424_blog";  
+$username = "if0_38872424";    
+$password = "HariAshish2223";
 
-$host = 'localhost';
-$db   = 'Blog';
-$user = 'root'; 
-$pass = null;  
-
-$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
