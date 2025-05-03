288b30cf30dddd818ee719897a530f894bdf3188
AchyutTiwari21 <achyut.s.tiwari@gmail.com>
2025-05-03 21:27:25 +0530

    Add cors.php and .htaccess

--- utils/cors.php
+++ utils/cors.php
@@ -3,12 +3,11 @@
 // Enable CORS for frontend deployed on Vercel
 header("Access-Control-Allow-Origin: https://blogging-application-three.vercel.app");
 header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
-    exit;
+    exit();
 }
-
