3ec3d53e2444a9ea8523744c1fc9782336c127e9
AchyutTiwari21 <achyut.s.tiwari@gmail.com>
2025-05-04 12:52:42 +0530

    Debug CORS error.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '/db/connection.php';
 require_once __DIR__ . '/utils/cors.php';
+require_once __DIR__ . '/db/connection.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
